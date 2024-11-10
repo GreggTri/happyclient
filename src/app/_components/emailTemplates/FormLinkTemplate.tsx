70f79137f23ad55c76c79749bdc88ea964c42d10
Gregg Trimarchi <gttrimarchig@gmail.com>
2024-11-09 20:09:44 -0700

    email templates are complete!

--- src/app/_components/emailTemplates/FormLinkTemplate.tsx
+++ src/app/_components/emailTemplates/FormLinkTemplate.tsx
@@ -1,103 +1,85 @@
 import {
-    Body,
-    Button,
-    Container,
-    Head,
-    Html,
-    Img,
-    //Link,
-    Preview,
-    Section,
-    Text,
-  } from "@react-email/components";
-  import * as React from "react";
-  
-  interface DropboxResetPasswordEmailProps {
-    userFirstname?: string;
-    resetPasswordLink?: string;
-  }
-  
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "";
-  
-  export const DropboxResetPasswordEmail = ({
-    userFirstname,
-    resetPasswordLink,
-  }: DropboxResetPasswordEmailProps) => {
-    return (
-      <Html>
-        <Head />
-        <Preview>Dropbox reset your password</Preview>
-        <Body style={main}>
-          <Container style={container}>
-            <Img
-              src={`${baseUrl}/static/dropbox-logo.png`}
-              width="40"
-              height="33"
-              alt="Dropbox"
-            />
-            <Section>
-              <Text style={text}>Hi {userFirstname},</Text>
-              <Text style={text}>
-                Someone recently requested a password change for your Dropbox
-                account. If this was you, you can set a new password here:
-              </Text>
-              <Button style={button} href={resetPasswordLink}>
-                Reset password
-              </Button>
-              <Text style={text}>
-                If you don&apos;t want to change your password or didn&apos;t
-                request this, just ignore and delete this message.
-              </Text>
-              <Text style={text}>
-                To keep your account secure, please do <b>NOT</b> forward this email
-                to anyone.
-              </Text>
-              <Text style={text}>Go make some happy clients!</Text>
-            </Section>
-          </Container>
-        </Body>
-      </Html>
-    );
-  };
-  
-  DropboxResetPasswordEmail.PreviewProps = {
-    userFirstname: "Alan",
-    resetPasswordLink: "https://dropbox.com",
-  } as DropboxResetPasswordEmailProps;
-  
-  export default DropboxResetPasswordEmail;
-  
-  const main = {
-    backgroundColor: "#f6f9fc",
-    padding: "10px 0",
-  };
-  
-  const container = {
-    backgroundColor: "#ffffff",
-    border: "1px solid #f0f0f0",
-    padding: "45px",
-  };
-  
-  const text = {
-    fontSize: "16px",
-    fontFamily:
-      "'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif",
-    fontWeight: "300",
-    color: "#404040",
-    lineHeight: "26px",
-  };
-  
-  const button = {
-    backgroundColor: "#007ee6",
-    borderRadius: "4px",
-    color: "#fff",
-    fontFamily: "'Open Sans', 'Helvetica Neue', Arial",
-    fontSize: "15px",
-    textDecoration: "none",
-    textAlign: "center" as const,
-    display: "block",
-    width: "210px",
-    padding: "14px 7px",
-  };
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+interface FormLinkTemplateProps {
+  surveyLink: string;
+  companyName: string;
+}
+
+export const FormLinkTemplate = ({
+  surveyLink,
+  companyName,
+}: FormLinkTemplateProps) => {
+  return (
+    <Html>
+      <Head />
+      <Preview>{`${companyName} Invites You To Share Your Experience`}</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={contentSection}>
+            <Text style={greeting}>Survey from {companyName}</Text>
+            <Text style={message}>
+              We appreciate you taking the time out of your day to help us grow
+              and provide an exceptional experience not only to you but to all
+              of our clients!
+            </Text>
+            <Text style={greeting}>Complete Your Survey Here:</Text>
+            <Button href={surveyLink}>{surveyLink}</Button>
+            <Text style={footer}>Thank you for your time and feedback!</Text>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default FormLinkTemplate;
+
+const main = {
+  backgroundColor: "#f9fafb",
+  padding: "20px 0",
+  fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+};
+
+const container = {
+  backgroundColor: "#ffffff",
+  borderRadius: "8px",
+  boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+  maxWidth: "600px",
+  margin: "0 auto",
+  padding: "40px",
+  textAlign: "center" as const,
+};
+
+const contentSection = {
+  textAlign: "center" as const,
+};
+
+const greeting = {
+  fontSize: "20px",
+  color: "#333333",
+  marginBottom: "10px",
+  fontWeight: "bold" as const,
+};
+
+const message = {
+  fontSize: "16px",
+  color: "#555555",
+  lineHeight: "1.6",
+  marginBottom: "30px",
+};
+
+const footer = {
+  fontSize: "14px",
+  color: "#777777",
+  marginTop: "20px",
+};
--- src/app/_components/emailTemplates/resetpassword.tsx
+++ src/app/_components/emailTemplates/resetpassword.tsx
@@ -1,103 +1,95 @@
 import {
-    Body,
-    Button,
-    Container,
-    Head,
-    Html,
-    Img,
-    //Link,
-    Preview,
-    Section,
-    Text,
-  } from "@react-email/components";
-  import * as React from "react";
-  
-  interface DropboxResetPasswordEmailProps {
-    userFirstname?: string;
-    resetPasswordLink?: string;
-  }
-  
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "";
-  
-  export const DropboxResetPasswordEmail = ({
-    userFirstname,
-    resetPasswordLink,
-  }: DropboxResetPasswordEmailProps) => {
-    return (
-      <Html>
-        <Head />
-        <Preview>Dropbox reset your password</Preview>
-        <Body style={main}>
-          <Container style={container}>
-            <Img
-              src={`${baseUrl}/static/dropbox-logo.png`}
-              width="40"
-              height="33"
-              alt="Dropbox"
-            />
-            <Section>
-              <Text style={text}>Hi {userFirstname},</Text>
-              <Text style={text}>
-                Someone recently requested a password change for your Dropbox
-                account. If this was you, you can set a new password here:
-              </Text>
-              <Button style={button} href={resetPasswordLink}>
-                Reset password
-              </Button>
-              <Text style={text}>
-                If you don&apos;t want to change your password or didn&apos;t
-                request this, just ignore and delete this message.
-              </Text>
-              <Text style={text}>
-                To keep your account secure, please do <b>NOT</b> forward this email
-                to anyone.
-              </Text>
-              <Text style={text}>Go make some happy clients!</Text>
-            </Section>
-          </Container>
-        </Body>
-      </Html>
-    );
-  };
-  
-  DropboxResetPasswordEmail.PreviewProps = {
-    userFirstname: "Alan",
-    resetPasswordLink: "https://dropbox.com",
-  } as DropboxResetPasswordEmailProps;
-  
-  export default DropboxResetPasswordEmail;
-  
-  const main = {
-    backgroundColor: "#f6f9fc",
-    padding: "10px 0",
-  };
-  
-  const container = {
-    backgroundColor: "#ffffff",
-    border: "1px solid #f0f0f0",
-    padding: "45px",
-  };
-  
-  const text = {
-    fontSize: "16px",
-    fontFamily:
-      "'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif",
-    fontWeight: "300",
-    color: "#404040",
-    lineHeight: "26px",
-  };
-  
-  const button = {
-    backgroundColor: "#007ee6",
-    borderRadius: "4px",
-    color: "#fff",
-    fontFamily: "'Open Sans', 'Helvetica Neue', Arial",
-    fontSize: "15px",
-    textDecoration: "none",
-    textAlign: "center" as const,
-    display: "block",
-    width: "210px",
-    padding: "14px 7px",
-  };
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+interface ResetPasswordEmailProps {
+  userFirstName?: string;
+  resetPasswordLink?: string;
+}
+
+export const ResetPasswordEmail = ({
+  userFirstName = "Gregg",
+  resetPasswordLink = "https://gethappyclient.com/forgot-password",
+}: ResetPasswordEmailProps) => {
+  return (
+    <Html>
+      <Head />
+      <Preview>HappyClient: reset your password</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          {/* <Img
+            src={`${process.env.BASE_DOMAIN}/public/assets/logo.png`}
+            
+            height={40}
+            alt="Happy Client"
+          /> */}
+          <Section>
+            <Text style={text}>Hi {userFirstName},</Text>
+            <Text style={text}>
+              Someone recently requested a password change for your <br />
+              <strong>Happy Client </strong>
+              account. If this was you, you can set a new password here:
+            </Text>
+            <Button style={button} href={resetPasswordLink}>
+              Reset password
+            </Button>
+            <Text style={text}>
+              If you don&apos;t want to change your password or didn&apos;t
+              request this, just ignore and delete this message.
+            </Text>
+            <Text style={text}>
+              To keep your account secure, please do <b>NOT</b> forward this
+              email to anyone.
+            </Text>
+            <Text style={text}>
+              <strong>Go make some happy clients!</strong>
+            </Text>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default ResetPasswordEmail;
+
+const main = {
+  backgroundColor: "#f6f9fc",
+  padding: "10px 0",
+};
+
+const container = {
+  backgroundColor: "#ffffff",
+  border: "1px solid #f0f0f0",
+  padding: "45px",
+};
+
+const text = {
+  fontSize: "16px",
+  fontFamily:
+    "'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif",
+  fontWeight: "300",
+  color: "#404040",
+  lineHeight: "26px",
+};
+
+const button = {
+  backgroundColor: "#f4e300",
+  borderRadius: "4px",
+  color: "#000",
+  fontFamily: "'Open Sans', 'Helvetica Neue', Arial",
+  fontSize: "15px",
+  textDecoration: "none",
+  textAlign: "center" as const,
+  display: "block",
+  width: "210px",
+  padding: "14px 7px",
+};
