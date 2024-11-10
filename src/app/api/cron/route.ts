429f338faff34db8608770be07bb15d1091f517f
Gregg Trimarchi <gttrimarchig@gmail.com>
2024-11-09 20:10:00 -0700

    cronjob route complete
    still need to do testing

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -9,148 +9,115 @@ import FormLinkTemplate from "@/app/_components/emailTemplates/FormLinkTemplate"
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function GET(request: NextRequest){
+export async function GET(request: NextRequest) {
     const authHeader = request.headers.get('authorization');
     if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
         return new Response('Unauthorized', {
-        status: 401,
+            status: 401,
         });
     }
 
-    const orgs = await prisma.org.findMany()
+    const orgs = await prisma.org.findMany();
 
-
-    for(const org of orgs){
-
+    for (const org of orgs) {
         const surveySendGroups = await prisma.surveySendGroup.findMany({
             where: {
-              isArchived: false,
-              tenantId: org.id
-            }
+                isArchived: false,
+                tenantId: org.id,
+            },
         });
-        
+
         // Filter groups that are scheduled for today based on scheduledSend or interval timing
         const todaySurveySendGroups = surveySendGroups.filter(group => isScheduledToday(group));
 
-        const clientEmailsofGroups = await prisma.surveySendEmail.findMany({
-            where: {
-                surveySendGroupId: {
-                    in: todaySurveySendGroups.map((group) => group.id)
-                }
+        for (const surveyGroup of todaySurveySendGroups) {
+            const surveyGroupId = surveyGroup.id;
+            const surveyId = surveyGroup.surveyId;
+
+            // Get clients in this surveyGroup
+            const clients = await prisma.surveySendEmail.findMany({
+                where: {
+                    surveySendGroupId: surveyGroupId,
+                },
+            });
+
+            const clientsData = [];
+
+            // Create surveyData entries and prepare email data
+            for (const client of clients) {
+                const token = generateToken();
+
+                // Create surveyData
+                const clientSurveyData = await prisma.surveyData.create({
+                    data: {
+                        tenantId: org.id,
+                        surveyId: surveyId,
+                        attachedUsers: surveyGroup.attachedUsers,
+                        expirationDate: surveyGroup.expirationDate,
+                        token: token,
+                    },
+                });
+
+                clientsData.push({
+                    email: client.email,
+                    surveyLink: `https://${org.domain}/${token}`,
+                    clientSurveyDataId: clientSurveyData.id,
+                    clientSurveyDataTenantId: clientSurveyData.tenantId,
+                });
             }
-        })
-        
-        for(const client of clientEmailsofGroups){
 
-            const result = await prisma.$transaction(async (prisma) => {
+            // Prepare batch emails
+            const batchEmails = clientsData.map(clientData => ({
+                from: `${org.companyName} <${org.surveyEmail}>`,
+                to: [clientData.email],
+                subject: `${org.companyName} sent you a survey!`,
+                react: FormLinkTemplate({ surveyLink: clientData.surveyLink, companyName: org.companyName }),
+            }));
 
-                //get surveyGroup from todaySurveySendGroups where client.surveysendgroupId = group.id of todaySurveySendGroups 
-                const surveyGroup = todaySurveySendGroups.find(group => group.id === client.surveySendGroupId);
+            // Send batch emails using resend.batch.send
+            try {
+                const batchEmailResponse = await resend.batch.send(batchEmails);
 
-                if (surveyGroup) {
-                    const surveyId = surveyGroup.surveyId;
-        
-                    // You can now use the surveyId for further processing, e.g., sending the email
-                    console.log(`Processing surveyId: ${surveyId} for client email: ${client.email}`);
-                    
-        
+                if (batchEmailResponse && batchEmailResponse.data && Array.isArray(batchEmailResponse.data)) {
+                    const batchEmailResults = batchEmailResponse.data;
 
-                    const clientSurveyData = await prisma.surveyData.create({
-                        data:{
-                            tenantId: org.id,
-                            surveyId: surveyId,
-                            attachedUsers: surveyGroup.attachedUsers,
-                            expirationDate: surveyGroup.expirationDate,
-                            token: generateToken()
-                        }
-                    })
+                    // Update surveyData entries with resendEmailIds
+                    for (let i = 0; i < batchEmailResults.length; i++) {
+                        const result = batchEmailResults[i];
+                        const clientData = clientsData[i];
 
-                    if (!clientSurveyData){
-                        console.log({
-                            'error': "could not create new clientSurveyData",
-                            'surveyId': surveyId,
-                            'tenantId': org.id,
-                            'client': client.email
-                        });
-                        throw new Error("could not create new clientSurveyData")
-                    }
+                        const resendEmailId = result.id; // Get the resendEmailId from the response
 
-                    //send email to client with template using org's email/domain stuff
-
-                    //TODO:: MAKE THIS A BATCH REQUEST for the entire org.
-                    const { data, error } = await resend.emails.send({
-                        from:`${org.companyName} <${org.surveyEmail}>`,
-                        to: [`${client.email}`],
-                        subject: `${org.companyName} would like you to fill out their survey!`,
-                        react: FormLinkTemplate({ userFirstname: 'John' }),
-                    });
-
-                    if (error) {
-                        console.log({
-                            'message': "Failed to Send email.",
-                            'clientEmail':  client.email,
-                            'tenantId': org.id,
-                            'surveyId': surveyId,
-                            'error': error
-                        });
-
-                        throw new Error(error.message)
-                    }
-
-                    if(!data){
-                        console.log("There was no error but data response from resend is empty")
-
-                        throw new Error("There was no error but data response from resend is empty")
-                    }
-
-                    //update clientSurveyData with ResendEmailId
-                    const updateClientSurveyData = await prisma.surveyData.update({
-                        where: {
-                            tenantId: clientSurveyData.tenantId,
-                            id: clientSurveyData.id
-                        },
-                        data: {
-                            resendEmailId: data.id
-                        }
-
-                    })
-
-                    if(!updateClientSurveyData){
-                        console.log({
-                            'message': "could not update resendEmailId for client",
-                            'clientEmail': client.email,
-                            'resendEmailId': data.id
+                        await prisma.surveyData.update({
+                            where: {
+                                id: clientData.clientSurveyDataId,
+                            },
+                            data: {
+                                resendEmailId: resendEmailId,
+                            },
                         });
                     }
                 } else {
-                    console.log({
-                        'message': "Seems that SurveyGroup is empty when comparing with client surveyGroups",
-                        'SurveyGroup': surveyGroup,
-                        'client': client
-                    });
-
-                    throw new Error("Seems that SurveyGroup is empty when comparing with client surveyGroups")
+                    console.error(`Unexpected response from resend.batch.send for org ${org.id}:`, batchEmailResponse);
+                    // Handle unexpected response
                 }
-            })
-
-            console.log(result);
+            } catch (error) {
+                console.error(`Failed to send batch emails for org ${org.id}:`, error);
+                // Handle error appropriately, possibly continue to next org or survey group
+            }
         }
-
     }
-    
 
     return NextResponse.json({
         message: "Cron Job Ran " + new Date(),
-    }, {status: 200})
-
+    }, { status: 200 });
 }
 
 function isScheduledToday(group: SurveySendGroup): boolean {
     const { scheduledSend, intervalTiming } = group;
     const today = new Date();
-    today.setHours(0, 0, 0, 0);  // Midnight comparison
-    
-    // Convert to a proper Date object if necessary
+    today.setHours(0, 0, 0, 0); // Midnight comparison
+
     const scheduledDate = new Date(scheduledSend);
 
     // Check if scheduledSend is today
@@ -160,7 +127,7 @@ function isScheduledToday(group: SurveySendGroup): boolean {
         return true;
     }
 
-    // If not today, calculate the next interval send date
+    // Calculate the next interval send date
     const nextSendDate = nextIntervalDate(scheduledSend, intervalTiming);
 
     // Check if the calculated next send date is today
@@ -169,45 +136,42 @@ function isScheduledToday(group: SurveySendGroup): boolean {
 
 function nextIntervalDate(InitialScheduleDate: Date, intervalTiming: string) {
     const today = new Date();
-
-    const nextDate = new Date(InitialScheduleDate);  // Start with the initial scheduled date
+    const nextDate = new Date(InitialScheduleDate); // Start with the initial scheduled date
 
     // Keep adding intervals until the nextDate is in the future
     while (nextDate <= today) {
         switch (intervalTiming) {
             case 'ONCE':
-                // If it's a one-time schedule and the date has passed, return null
-                return nextDate > today ? nextDate : "never";
+                return nextDate > today ? nextDate : null;
 
             case 'WEEKLY':
-                nextDate.setDate(nextDate.getDate() + 7);  // Add 7 days
+                nextDate.setDate(nextDate.getDate() + 7); // Add 7 days
                 break;
-                
+
             case 'BIWEEKLY':
-                nextDate.setDate(nextDate.getDate() + 14);  // Add 14 days
+                nextDate.setDate(nextDate.getDate() + 14); // Add 14 days
                 break;
-                
+
             case 'MONTHLY':
-                nextDate.setMonth(nextDate.getMonth() + 1);  // Add 1 month
+                nextDate.setMonth(nextDate.getMonth() + 1); // Add 1 month
                 break;
-                
+
             case 'QUARTERLY':
-                nextDate.setMonth(nextDate.getMonth() + 3);  // Add 3 months
+                nextDate.setMonth(nextDate.getMonth() + 3); // Add 3 months
                 break;
-                
+
             case 'SEMIANNUALY':
-                nextDate.setMonth(nextDate.getMonth() + 6);  // Add 6 months
+                nextDate.setMonth(nextDate.getMonth() + 6); // Add 6 months
                 break;
-                
+
             case 'ANNUALY':
-                nextDate.setFullYear(nextDate.getFullYear() + 1);  // Add 1 year
+                nextDate.setFullYear(nextDate.getFullYear() + 1); // Add 1 year
                 break;
-                
+
             default:
                 throw new Error(`Unknown interval timing: ${intervalTiming}`);
         }
     }
-    
-    return nextDate.toDateString()
+
+    return nextDate;
 }
-  
